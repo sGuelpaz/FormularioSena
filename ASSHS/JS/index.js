f789950ad8bd5d3a60bc86355b8aa6de8d36d260
sGuelpaz <jhon.guelpaz@misena.edu.co>
2021-11-25 22:22:36 -0500

    Formulario Js a Consola Fin

--- ASSHS/JS/index.js
+++ ASSHS/JS/index.js
@@ -1,5 +1,37 @@
-let valTextBox = document.getElementById('textBox').value;
-let valTextBox = document.getElementById('textBox').value;
-let valTextBox = document.getElementById('textBox').value;
-let valTextBox = document.getElementById('textBox').value;
-let valTextBox = document.getElementById('textBox').value;
+
+
+var getData =function(){
+    var name = document.getElementById("nombre").value;
+    var lastname = document.getElementById("apellido").value;
+    var tipo_documento = document.querySelector('#formulario1 [name=documento]:checked').value;
+    var num_doc = document.getElementById("num_doc").value;
+    var email = document.getElementById("email").value;
+    var observation = document.getElementById("observacion").value;
+    console.log("Nombre completo: "+name+" "+lastname);
+    console.log("Tipo de documento: "+tipo_documento+" Número: " +num_doc);
+    console.log("Email: "+email);
+    console.log("Obervaciones: "+observation);
+
+     /*  function validarRadio(){
+        var valid = 1;
+        for (var i=0;i<document.formulario.documento.length;i++)
+        {
+            if (document.formulario.documento[i].checked){
+                if (i=0){
+                    var tipo_documento = "C_C";
+                }
+                if (i=1){
+                    var tipo_documento = "T.I";
+                }
+                if (i=2){
+                    var tipo_documento = "Pasaporte";
+                }
+                if (i=3){
+                    var tipo_documento = "PEP";
+                }
+            }
+            
+        }
+    }*/
+    
+}
